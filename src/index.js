ba61792fc1e408a4d362b3b16dc2deb792f7d872
heuzin <matheusqs.arq@gmail.com>
2021-02-14 17:35:43 -0300

    form for adding a recipe into recipes array

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import ReactDOM from 'react-dom'
 
+const recipes = []
+
+const onFormSubmit = (e) => {
+    e.preventDefault()
+    
+    const recipe = e.target.elements.recipes.value
+
+    if (recipe) {
+        recipes.push(recipe)
+        e.target.elements.recipes.value = ''
+        renderRecipeApp()
+    }
+}
+
 const appRoot = document.getElementById('app')
 
 const renderRecipeApp = () => {
     const template = (
         <div>
             <h1>Recipe App</h1>
+            <p>{recipes.length}</p>
             <ol>
                 <li>Pizza</li>
                 <li>Lasanha</li>
                 <li>Pasta</li>
             </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='recipes'/>
+                <button>Add Recipe</button>
+            </form>
         </div>
     );
 
