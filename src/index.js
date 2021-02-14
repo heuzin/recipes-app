dfc0a09e6dd03b66da7c1b238477c7d6a8834667
heuzin <matheusqs.arq@gmail.com>
2021-02-14 17:31:23 -0300

    creating a function to render recipe app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom'
 
-const template = (
-    <div>
-        <h1>Recipe App</h1>
-        <ol>
-            <li>Pizza</li>
-            <li>Lasanha</li>
-            <li>Pasta</li>
-        </ol>
-    </div>
-);
-
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+const renderRecipeApp = () => {
+    const template = (
+        <div>
+            <h1>Recipe App</h1>
+            <ol>
+                <li>Pizza</li>
+                <li>Lasanha</li>
+                <li>Pasta</li>
+            </ol>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot)
+}
+
+renderRecipeApp()
