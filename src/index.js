432997d9efe0108d88235333593726160ad67af8
heuzin <matheusqs.arq@gmail.com>
2021-02-14 11:40:42 -0300

    rendering template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('app')
+import ReactDOM from 'react-dom'
+
+const template = <p>This is my JSX template</p>
+
+const appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
