ecc571d071ca4a20b67b48d914b4032138c7ac70
heuzin <matheusqs.arq@gmail.com>
2021-02-14 12:03:12 -0300

    fixing an error on index.html, added header and an orded list into template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom'
 
-const template = <p>This is my JSX template</p>
+const template = (
+    <div>
+        <h1>Recipe App</h1>
+        <ol>
+            <li>Pizza</li>
+            <li>Lasanha</li>
+            <li>Pasta</li>
+        </ol>
+    </div>
+);
 
 const appRoot = document.getElementById('app')
 
